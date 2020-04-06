350c4b437889e72fd3599feace9f32d5f3b5eae5
Alba <alba.lopez@nfq.es>
2020-04-06 17:50:50 +0200

    add structure of the website

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-card class="overflow-hidden">
+      <Header />
+      <v-sheet id="scrolling-techniques-2" class="overflow-y-auto" max-height="100vh">
+        <v-container style="height: auto; margin-top: 128px;">
+          <v-content>
+            <HelloWorld/>
+          </v-content>
+        </v-container>
+      </v-sheet>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import Header from './components/Header';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    Header
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,85 @@
+<template>
+<div>
+  <v-app-bar
+    absolute
+    color="#fcb69f"
+    dark
+    shrink-on-scroll
+    src="../assets/background-header.jpg"
+    scroll-target="#scrolling-techniques-2"
+  >
+    <template v-slot:img="{ props }">
+      <v-img v-bind="props" gradient="to top right, rgba(173,140,106,.5), rgba(173,140,106,.8)"></v-img>
+    </template>
+
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="d-lg-none"></v-app-bar-nav-icon>
+
+    <v-toolbar-title>
+      <v-img
+        :src="require('../assets/logoA.png')"
+        contain
+        height="40"
+        width="40"
+        style="border-radius: 100%"
+      />
+      <span>Alba López Folgar</span>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    <v-btn text href="#about" class="d-none d-lg-flex">About</v-btn>
+    <v-btn text href="#projects"  class="d-none d-lg-flex">Projects</v-btn>
+    <v-btn text href="#experience"  class="d-none d-lg-flex">Education and experience</v-btn>
+    <v-btn text href="#contact"  class="d-none d-lg-flex">Contact</v-btn>
+
+  </v-app-bar>
+  <v-navigation-drawer v-model="drawer" absolute temporary>
+    <v-list nav dense>
+      <v-list-item-group active-class="orange lighten-3--text text--accent-4">
+        <v-list-item href="#about" @click.stop="drawer = !drawer">
+          <v-list-item-title>About</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="#projects" @click.stop="drawer = !drawer">
+          <v-list-item-title>Projects</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="#experience" @click.stop="drawer = !drawer">
+          <v-list-item-title >Education and experience</v-list-item-title>
+        </v-list-item>
+        <v-list-item href="#contact" @click.stop="drawer = !drawer">
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {},
+  data: () => ({
+    drawer: false
+  })
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.v-btn {
+  background-color: none !important;
+  box-shadow: none;
+}
+.theme--dark.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined) {
+  background-color: none !important;
+}
+.v-toolbar.v-toolbar--absolute {
+  z-index: 5;
+}
+.v-toolbar__title {
+  display: flex;
+  align-items: center;
+}
+.v-toolbar__title .v-image {
+  margin-right: 10px;
+}
+</style>
